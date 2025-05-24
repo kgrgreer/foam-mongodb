9276a111a53c9a31dad7497d65ee67e8b2e31764
Joel Hughes <joel@nanopay.net>
2025-05-24 09:28:31 -0400

    tooling - update test.sh syntax regarding test cases to match foam's updated build tooling

--- deployment/test/test.sh
+++ deployment/test/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-node foam3/tools/build.js -Pfoam3/pom,pom -Jtest -TMongoDAOTest,MongoDAOTestBenchmark -LINFO -c "$@"
+node foam3/tools/build.js -Pfoam3/pom,pom -Jtest --java-tests:MongoDAOTest,MongoDAOTestBenchmark --log-level:INFO -c "$@"
